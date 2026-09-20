929994cd7d340c3201f2dafab7bd5bb6c94ebca4
panumad wanyat <panumadwanyat@gmail.com>
2026-09-20 16:24:13 +0700

    Update meteo_fame.java

--- meteo_fame.java
+++ meteo_fame.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class meteo_fame extends JFrame {
-    public MyFame() {
-        setSize(500,500);
-        setDefaultCloseOperation(JFame.EXIT_ON_CLOSE);
+
+    public meteo_fame() {
+
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        setIconImage();
+        ImageIcon icon = new ImageIcon("background.jpg");
+        JLabel background = new JLabel(icon);
+
+        background.setBounds(0, 0, 500, 500);
+
+        add(background);
 
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new meteo_fame();
     }
 }
